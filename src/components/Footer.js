87066484b6cdabf09f2397a34e5f7ea493c28755
Tom Garrett <85767932+tomgarrett95@users.noreply.github.com>
2021-12-07 17:12:43 +0000

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="flex justify-center bg-teal text-offWhite py-2">
-      <p>&copy;2021 Tom Garrett</p>
-    </footer>
-  )
-}
