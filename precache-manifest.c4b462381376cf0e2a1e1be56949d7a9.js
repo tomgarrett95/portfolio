f1eb2ead03acfe78b46016f5e3ad3bd712a3e48a
tomgarrett95 <tgarrett.tech@gmail.com>
2022-01-22 18:48:27 +0000

    Updates

---
+++ precache-manifest.c4b462381376cf0e2a1e1be56949d7a9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d280fb25a1f713b5160b0ff28dd015",
+    "url": "/index.html"
+  },
+  {
+    "revision": "563ea31f207db29dae81",
+    "url": "/static/css/main.1f5f6264.chunk.css"
+  },
+  {
+    "revision": "4057849456461656886f",
+    "url": "/static/js/2.0d191f94.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.0d191f94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1fa2beb6c86a187636e",
+    "url": "/static/js/3.551c8a5c.chunk.js"
+  },
+  {
+    "revision": "563ea31f207db29dae81",
+    "url": "/static/js/main.462ff45a.chunk.js"
+  },
+  {
+    "revision": "5526f739107106eb3b7e",
+    "url": "/static/js/runtime-main.8cc252b1.js"
+  },
+  {
+    "revision": "8a2c2459d6cc783339e8fd446793afb3",
+    "url": "/static/media/TG-logo.8a2c2459.png"
+  },
+  {
+    "revision": "70bc4bb71350a7595826ee79d055a0a0",
+    "url": "/static/media/academy-aptitude-test.70bc4bb7.png"
+  },
+  {
+    "revision": "b0a4f4a059c1a482ae196f7b3e7fd6ee",
+    "url": "/static/media/academy-portal.b0a4f4a0.png"
+  },
+  {
+    "revision": "8b058936ea13e2290d32227c8d87ae9a",
+    "url": "/static/media/cat.8b058936.png"
+  },
+  {
+    "revision": "3fce9abd498a7df07b84ac52b4b69627",
+    "url": "/static/media/coffee.3fce9abd.png"
+  },
+  {
+    "revision": "54493e2d795cb462f790cc263926b69d",
+    "url": "/static/media/controller.54493e2d.png"
+  },
+  {
+    "revision": "3591f1b434dab7ea061715384d65d416",
+    "url": "/static/media/map.3591f1b4.png"
+  },
+  {
+    "revision": "e3293ec424928be76c198edbce6009b0",
+    "url": "/static/media/notld.e3293ec4.png"
+  },
+  {
+    "revision": "a55873a430e79393f37d4defa07b8faa",
+    "url": "/static/media/teal2.a55873a4.jpg"
+  },
+  {
+    "revision": "c50861de14c7384f94f4236cc0793a16",
+    "url": "/static/media/time-tracking-dashboard.c50861de.png"
+  },
+  {
+    "revision": "18bb7ac3695795e4a2ac947011458089",
+    "url": "/static/media/tom-garrett-cv.18bb7ac3.pdf"
+  },
+  {
+    "revision": "13c0e4c7001643904fb4a38ec47aa373",
+    "url": "/static/media/transformers-app.13c0e4c7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e05f260ec562d1d146959a59105bbd24.js"
+  "/precache-manifest.c4b462381376cf0e2a1e1be56949d7a9.js"
 );
 
 self.addEventListener('message', (event) => {
